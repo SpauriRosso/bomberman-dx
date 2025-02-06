058dab031e4d177e4400ae782a1b6457bf9dccb4
vlouvier <louvier.valentyn@gmail.com>
2025-02-06 14:54:09 +0100

    js

--- js/collision.js
+++
@@ -1,8 +0,0 @@
-class Collision {
-  static checkCollision(x, y) {
-    const tile = Game.tilemap.getTileAt(x, y);
-    return (
-      tile.classList.contains("wall") || tile.classList.contains("breakable")
-    );
-  }
-}
