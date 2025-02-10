e66f4045b977c237280b594085cd58d6f94e728a
SpauriRosso <spy456@zoho.com>
2025-02-10 14:43:42 +0100

    implemented mock code for CollisionSystem.js

--- systems/CollisionSystem.js
+++ systems/CollisionSystem.js
@@ -1,29 +1,41 @@
-class CollisionSystem {
+export default class CollisionSystem {
+  constructor (entities) {
+    this.entities = entities;
+  }
 
-  update() {
-    this.entities.forEach((entity) => {
-      const positionComponent = entity.getComponent("PositionComponent");
-      const healthComponent = entity.getComponent("HealthComponent");
-
-      if (positionComponent && healthComponent) {
-        // Vérifiez les collisions avec les autres entités
-        this.entities.forEach((otherEntity) => {
-          const otherPositionComponent = otherEntity.getComponent("PositionComponent");
-
-          if (otherPositionComponent) {
-            // Vérifiez si les entités se chevauchent
-            if (
-              positionComponent.x < otherPositionComponent.x + 64 &&
-              positionComponent.x + 64 > otherPositionComponent.x &&
-              positionComponent.y < otherPositionComponent.y + 64 &&
-              positionComponent.y + 64 > otherPositionComponent.y
-            ) {
-              // Appliquez les dégâts
-              healthComponent.health -= 10;
-            }
-          }
-        });
-      }
-    });
+  update(entities) {
+    entities.forEach((entity) => {
+      const position = entities.position;
+    })
   }
 }
+
+// class CollisionSystem {
+//
+//   update() {
+//     this.entities.forEach((entity) => {
+//       const positionComponent = entity.getComponent("PositionComponent");
+//       const healthComponent = entity.getComponent("HealthComponent");
+//
+//       if (positionComponent && healthComponent) {
+//         // Vérifiez les collisions avec les autres entités
+//         this.entities.forEach((otherEntity) => {
+//           const otherPositionComponent = otherEntity.getComponent("PositionComponent");
+//
+//           if (otherPositionComponent) {
+//             // Vérifiez si les entités se chevauchent
+//             if (
+//               positionComponent.x < otherPositionComponent.x + 64 &&
+//               positionComponent.x + 64 > otherPositionComponent.x &&
+//               positionComponent.y < otherPositionComponent.y + 64 &&
+//               positionComponent.y + 64 > otherPositionComponent.y
+//             ) {
+//               // Appliquez les dégâts
+//               healthComponent.health -= 10;
+//             }
+//           }
+//         });
+//       }
+//     });
+//   }
+// }
