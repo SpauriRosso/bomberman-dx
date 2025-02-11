3ef4d0966234746c578c77d51a2787e2d11a1e24
vlouvier <louvier.valentyn@gmail.com>
2025-02-11 18:40:36 +0100

    Components/

--- Components/BombDataComponent.js
+++ Components/BombDataComponent.js
@@ -1,12 +1,10 @@
-class BombDataComponent {
+export default class BombDataComponent {
   constructor() {
     this.explosionRange = 2;
     this.timer = 3;
   }
 
   setExplosionRange(n) {
-    this.explosionRange = n
+    this.explosionRange = n;
   }
 }
-
-export default BombDataComponent;
