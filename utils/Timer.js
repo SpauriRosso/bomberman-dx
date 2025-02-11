92bccbea162a8af93bbd4ef46dc1fbb151a85c94
vlouvier <louvier.valentyn@gmail.com>
2025-02-11 11:59:13 +0100

    script for the timer

--- utils/Timer.js
+++ utils/Timer.js
@@ -1,51 +1,43 @@
-class Timer {
-  constructor() {
-    this.seconds = 0;
-    this.minutes = 0;
+export default class Timer {
+  /**
+   * Initialize the timer with an optional start value.
+   * @param {number} [startValue=0] - The initial value of the timer.
+   */
+  constructor(startValue = 0) {
+    this.startValue = startValue;
+    this.currentTime = startValue;
     this.intervalId = null;
-    this.isRunning = false;
   }
 
-  start() {
-    if (!this.isRunning) {
-      this.isRunning = true;
-      this.intervalId = setInterval(() => {
-        this.increment();
-        this.display();
-      }, 1000);
-    }
+  /**
+   * Start the timer.
+   * @param {number} [interval=1000] - The interval in milliseconds between increments.
+   */
+  start(interval = 1000) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, interval);
   }
 
+  /**
+   * Stop the timer.
+   */
   stop() {
-    if (this.isRunning) {
-      clearInterval(this.intervalId);
-      this.isRunning = false;
-    }
+    clearInterval(this.intervalId);
   }
 
+  /**
+   * Get the current time.
+   * @returns {number} The current time.
+   */
+  getTime() {
+    return this.currentTime;
+  }
+
+  /**
+   * Reset the timer to its start value.
+   */
   reset() {
-    this.stop();
-    this.seconds = 0;
-    this.minutes = 0;
-    this.display();
-  }
-
-  increment() {
-    this.seconds++;
-    if (this.seconds >= 60) {
-      this.seconds = 0;
-      this.minutes++;
-    }
-  }
-
-  display() {
-    const timeString = `${this.formatTime(this.minutes)}:${this.formatTime(
-      this.seconds
-    )}`;
-    document.getElementById("timer").textContent = timeString;
-  }
-
-  formatTime(num) {
-    return num.toString().padStart(2, "0");
+    this.currentTime = this.startValue;
   }
 }
