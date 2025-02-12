4560d7b421cfaf7919280be3f0b73b24be9f7874
vlouvier <louvier.valentyn@gmail.com>
2025-02-12 13:28:33 +0100

    Components/

--- Components/BombDataComponent.js
+++ Components/BombDataComponent.js
@@ -4,8 +4,23 @@ export default class BombDataComponent {
     this.explosionRange = 2;
     this.timer = 3;
     this.timerInterval = 1000;
+    this.x = 0;
+    this.y = 0;
+    this.width = 64; // Assuming the bomb is 64x64 pixels
+    this.height = 64;
+    this.explosionRadius = 2; // The radius of the explosion
+    this.explosionTime = 2000; // The time it takes for the bomb to explode (in milliseconds)
+    this.timer = new Timer();
+  }
+  update() {
+    // Update the bomb's timer
+    this.timer.update();
   }
 
+  explode() {
+    // Handle the explosion logic here
+    console.log("Bomb exploded!");
+  }
   setExplosionRange(n) {
     this.explosionRange = n;
   }
