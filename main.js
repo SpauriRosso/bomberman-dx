8a30062e9d0d83515f962d5abc6f761822161bc6
vlouvier <louvier.valentyn@gmail.com>
2025-02-12 14:56:50 +0100

    main.js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ import { generateGrid, findPlayerPosition } from "./utils/tileMap.js";
 import MovementSystem from "./systems/MovementSystem.js";
 import Timer from "./utils/Timer.js";
 import FPS from "./utils/FPS.js";
+import { tileMapDefault } from "./utils/tileMap.js";
+import CollisionSystem from "./systems/CollisionSystem.js";
+import BombSystem from "./systems/ BombSystem.js"; // Import BombSystem
+import BombEntity from "./entities/BombEntity.js"; // Import BombEntity
 
 // Get the timer element from the HTML document
 const timerElement = document.getElementById("timer");
@@ -22,30 +26,46 @@ const fps = new FPS();
 
 function animate(time) {
   fps.update(time);
-
   requestAnimationFrame(animate);
 }
 
 animate();
-// Get the current time every second
 
-import { tileMapDefault } from "./utils/tileMap.js";
-import CollisionSystem from "./systems/CollisionSystem.js";
-
-const collisionSystem = new CollisionSystem(tileMapDefault); // Initialisation
-const movementSystem = new MovementSystem(collisionSystem); // On passe collisionSystem
+const collisionSystem = new CollisionSystem(tileMapDefault);
+const movementSystem = new MovementSystem(collisionSystem);
 
 generateGrid();
 let { x, y } = findPlayerPosition();
 
 const gameLogicSystem = new GameLogicSystem();
-const player = new PlayerEntity(10, x * 64, y * 64); // Initialize player entity with position based on tilemap
+const player = new PlayerEntity(10, x * 64, y * 64);
 
 gameLogicSystem.addEntity(player);
-// gameLogicSystem.addSystem(new MovementSystem());
 gameLogicSystem.addSystem(new RenderSystem());
 gameLogicSystem.addSystem(movementSystem);
+
+const bombSystem = new BombSystem(); // Initialize BombSystem
+gameLogicSystem.addSystem(bombSystem); // Add BombSystem to the game
+
 console.log(gameLogicSystem);
+
+// Function to place a bomb (you can call this when the player presses a key)
+function placeBomb() {
+  const playerPos = player.getComponent("PositionComponent");
+
+  if (playerPos) {
+    const bomb = new BombEntity(`bomb_${Date.now()}`, playerPos.x, playerPos.y);
+    bombSystem.addBomb(bomb);
+    console.log("Bomb placed at:", playerPos.x, playerPos.y);
+  }
+}
+
+// Listen for a key press to place bombs (e.g., spacebar)
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    placeBomb();
+  }
+});
 
 function gameLoop() {
   gameLogicSystem.update();
@@ -59,6 +79,7 @@ function gameLoop() {
 
   requestAnimationFrame(gameLoop);
 }
+
 console.log("player position on the map", findPlayerPosition());
 
 requestAnimationFrame(gameLoop);
