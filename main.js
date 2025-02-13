c3594e062f9dcb795ffade44e789ad463af5195d
vlouvier <louvier.valentyn@gmail.com>
2025-02-13 17:52:17 +0100

    main.js

--- main.js
+++ main.js
@@ -1,30 +1,17 @@
-// main.js
+import Timer from "./utils/Timer.js"; // Import Timer
 import PlayerEntity from "./entities/PlayerEntity.js";
 import BombSystem from "./systems/ BombSystem.js";
 import GameLogicSystem from "./systems/GameLogicSystem.js";
 import RenderSystem from "./systems/RenderSystem.js";
 import { generateGrid, findPlayerPosition } from "./utils/tileMap.js";
 import MovementSystem from "./systems/MovementSystem.js";
-import Timer from "./utils/Timer.js";
 import FPS from "./utils/FPS.js";
 import { tileMapDefault } from "./utils/tileMap.js";
 import GridUpdateSystem from "./systems/GridUpdateSystem.js";
 import CollisionSystem from "./systems/CollisionSystem.js";
 
-// Get the timer element from the HTML document
-const timerElement = document.getElementById("timer");
-
-// Initialize the game timer with a start value of 0
-const timer = new Timer();
-timer.start();
-
-// Function to pad numbers with leading zeros for 2-digit numbers
-function padZero(number) {
-  return (number < 10 ? "0" : "") + number;
-}
-
 const fps = new FPS();
-
+const timer = new Timer();
 function animate(time) {
   fps.update(time);
   requestAnimationFrame(animate);
@@ -54,16 +41,10 @@ gameLogicSystem.addSystem(gridUpdateSystem);
 
 console.log(gameLogicSystem);
 
-// Function to place a bomb (you can call this when the player presses a key)
+// Game Loop
 function gameLoop() {
   gameLogicSystem.update();
-
-  // Update timer display
-  const currentTime = timer.getTime();
-  const minutes = Math.floor(currentTime / 60);
-  const seconds = currentTime % 60;
-  const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
-  timerElement.textContent = `TIME ${formattedTime}`;
+  timer.update(); // Update Timer
 
   requestAnimationFrame(gameLoop);
 }
