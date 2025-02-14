4f3aa732959afad717d503dbec16cf64b4e41a71
vlouvier <louvier.valentyn@gmail.com>
2025-02-14 18:03:37 +0100

    Components/

--- Components/EnemyComponent.js
+++ Components/EnemyComponent.js
@@ -1,33 +1,31 @@
-
+// export default class EnemyComponent {
+//   constructor() {
+//     this.aiBehavior = "random";
+//   }
 export default class EnemyComponent {
   constructor() {
     this.aiBehavior = "random";
+    this.x = 0;
+    this.y = 0;
+
+    this.moves = [
+      { x: 0, y: -1 }, // Up
+      { x: 0, y: 1 }, // Down
+      { x: -1, y: 0 }, // Left
+      { x: 1, y: 0 }, // Right
+    ];
   }
-class EnemyComponent {
-    constructor() {
-        this.aiBehavior = "random";
-        this.x = 0
-        this.y = 0
+  /**
+   * Generate random movement
+   * @return {object} - {x, y} for movement attempt
+   */
+  getRandMove() {
+    return this.moves[Math.floor(Math.random() * this.moves.length)];
+  }
 
-        this.moves = [
-            {x: 0, y: -1}, // Up
-            {x: 0, y: 1},  // Down
-            {x: -1, y: 0}, // Left
-            {x: 1, y: 0}   // Right
-        ];
-    }
-    /**
-     * Generate random movement
-     * @return {object} - {x, y} for movement attempt
-     */
-    getRandMove() {
-        return this.moves[Math.floor(Math.random() * this.moves.length)];
-    }
-
-    update() {
-      let move = this.getRandMove()
-      this.x = move.x;
-      this.y = move.y;
-    }
-
+  update() {
+    let move = this.getRandMove();
+    this.x = move.x;
+    this.y = move.y;
+  }
 }
--- Components/HealthComponent.js
+++ Components/HealthComponent.js
@@ -1,24 +1,20 @@
+// export default class HealthComponent {
+//   constructor(health) {
+//     this.health = health;
+//   }
+// }
 
 export default class HealthComponent {
   constructor(health) {
     this.health = health;
   }
+
+  damage(amount) {
+    this.health -= amount;
+    return this.health;
+  }
+
+  addLives() {
+    return this.health + 1;
+  }
 }
-
-class HealthComponent {
-    constructor(health) {
-        this.health = health;
-    }
-
-    damage(amount) {
-        this.health -= amount;
-        return this.health;
-    }
-
-    addLives() {
-        return this.health + 1;
-    }
-}
-
-export default HealthComponent;
-
