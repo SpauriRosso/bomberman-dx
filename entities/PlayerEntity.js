3eb250eb1e5ae2f1e4d3b0a88bb1068008c102b8
vlouvier <louvier.valentyn@gmail.com>
2025-02-17 10:55:15 +0100

    entities/

--- entities/PlayerEntity.js
+++ entities/PlayerEntity.js
@@ -5,21 +5,15 @@ import HealthComponent from "../Components/HealthComponent.js";
 import PlayerDataComponent from "../Components/PlayerDataComponent.js";
 import inputsComponent from "../Components/inputsComponents.js";
 import SpriteComponent from "../Components/spriteCommponent.js";
-import BombDataComponent from "../Components/BombDataComponent.js";
 
 class PlayerEntity extends Entity {
   constructor(id, x, y) {
     super(id);
-
     let velocityComponent = new VelocityComponent(0, 0);
     this.addComponent("position", new PositionComponent(x, y));
     this.addComponent("velocity", velocityComponent);
-    let positionComponent = new PositionComponent(x, y);
-    this.addComponent("position", positionComponent);
-    this.addComponent("velocity", new VelocityComponent(0, 0));
     this.addComponent("health", new HealthComponent(100));
     this.addComponent("data", new PlayerDataComponent());
-    this.addComponent("bomb", new BombDataComponent());
 
     let spriteComponent = new SpriteComponent(
       {
