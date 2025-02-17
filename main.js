00c743ca687ddbb8c1147c92184d7b970d240241
vlouvier <louvier.valentyn@gmail.com>
2025-02-17 10:56:18 +0100

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import Timer from "./utils/Timer.js"; // Import Timer
+/*const timer = new Timer();
+timer.start();
+
+const scoreManager = new ScoreManager();*/
+
 import PlayerEntity from "./entities/PlayerEntity.js";
-import BombSystem from "./systems/ BombSystem.js";
 import GameLogicSystem from "./systems/GameLogicSystem.js";
 import RenderSystem from "./systems/RenderSystem.js";
 import {
@@ -9,28 +12,16 @@ import {
   findEnemyPosition,
 } from "./utils/tileMap.js";
 import MovementSystem from "./systems/MovementSystem.js";
-import FPS from "./utils/FPS.js";
-import HUDHeader from "./utils/HUDheader.js";
+
 import { tileMapDefault } from "./utils/tileMap.js";
-import GridUpdateSystem from "./systems/GridUpdateSystem.js";
 import CollisionSystem from "./systems/CollisionSystem.js";
 import EnemyEntity from "./entities/EnemyEntity.js";
 
+const collisionSystem = new CollisionSystem(tileMapDefault); // Initialisation
+const movementSystem = new MovementSystem(collisionSystem);
+
 export let player;
 export let gameLogicSystem;
-
-const header = new HUDHeader(document.body, "header-image.png");
-const fps = new FPS();
-const timer = new Timer();
-function animate(time) {
-  fps.update(time);
-  requestAnimationFrame(animate);
-}
-
-animate();
-
-const collisionSystem = new CollisionSystem(tileMapDefault);
-const movementSystem = new MovementSystem(collisionSystem);
 
 generateGrid();
 
@@ -55,24 +46,13 @@ player = new PlayerEntity(10, x * 64, y * 64); // Initialize player entity with 
 gameLogicSystem.addEntity(player);
 gameLogicSystem.addSystem(new RenderSystem());
 gameLogicSystem.addSystem(movementSystem);
-
-const bombSystem = new BombSystem();
-gameLogicSystem.addSystem(bombSystem);
-
-const gridUpdateSystem = new GridUpdateSystem();
-gridUpdateSystem.update(tileMapDefault);
-gameLogicSystem.addSystem(gridUpdateSystem);
-
 console.log(gameLogicSystem);
 
-// Game Loop
 function gameLoop() {
   gameLogicSystem.update();
-  timer.update(); // Update Timer
 
   requestAnimationFrame(gameLoop);
 }
-
 console.log("player position on the map", findPlayerPosition());
 
 requestAnimationFrame(gameLoop);
