9397a07018c8bea8b6a0d512c5b471dd3663133a
vlouvier <louvier.valentyn@gmail.com>
2025-02-17 10:55:03 +0100

    Components/

--- Components/HealthComponent.js
+++ Components/HealthComponent.js
@@ -1,22 +1,6 @@
-// export default class HealthComponent {
-//   constructor(health) {
-//     this.health = health;
-//   }
-// }
-
 class HealthComponent {
   constructor(health) {
     this.health = health;
   }
-
-  damage(amount) {
-    this.health -= amount;
-    return this.health;
-  }
-
-  addLives() {
-    return this.health + 1;
-  }
 }
-
 export default HealthComponent;
--- Components/PlayerDataComponent.js
+++ Components/PlayerDataComponent.js
@@ -1,28 +1,6 @@
-import HealthComponent from "./HealthComponent.js";
-
 class PlayerDataComponent {
   constructor() {
     this.score = 0;
-    this.health = 3;
-    this.healthComponent = new HealthComponent(this.health);
-  }
-
-  receiveDamage(amount) {
-    return this.healthComponent.damage(amount);
-  }
-
-  heal(amount) {
-    return this.healthComponent.addLives(amount);
-  }
-
-  addScore(amount) {
-    this.score += amount;
-    return this.score;
-  }
-
-  removeScore(amount) {
-    this.score -= amount;
-    return this.score;
   }
 }
 
