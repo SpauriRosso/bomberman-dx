abaffb48c6ee9121e975edf9aa3b1e7cc9b99848
vlouvier <louvier.valentyn@gmail.com>
2025-02-19 12:52:48 +0100

    systems/

--- systems/CollisionSystem.js
+++ systems/CollisionSystem.js
@@ -3,6 +3,7 @@ import { tileMapDefault } from "../utils/tileMap.js";
 export default class CollisionSystem {
   constructor() {
     this.tileMap = tileMapDefault; // Stock game level
+    this.hitboxPadding = 2; // Padding around hitbox to prevent clipping
   }
 
   /**
@@ -12,16 +13,34 @@ export default class CollisionSystem {
    * @returns {boolean} - True if player can move, false otherwise
    */
   isCollide(x, y) {
-    const tileSize = 64; // Taille d'une tuile
-    const hitboxSize = 48; // Taille de la hitbox réduite
-    const offset = (tileSize - hitboxSize) / 2; // Décalage pour centrer la hitbox
+    const tileSize = 64; // Tile size
+    const hitboxSize = 48; // Reduced hitbox size
+    const offset = (tileSize - hitboxSize) / 2; // Offset to center hitbox
+    const padding = this.hitboxPadding; // Collision padding
 
     // Nouvelle liste de points de collision avec la hitbox réduite
+    // More precise collision points with padding
     const checkPoints = [
-      { x: x + offset, y: y + offset }, // Haut gauche réduit
-      { x: x + offset + hitboxSize - 1, y: y + offset }, // Haut droit réduit
-      { x: x + offset, y: y + offset + hitboxSize - 1 }, // Bas gauche réduit
-      { x: x + offset + hitboxSize - 1, y: y + offset + hitboxSize - 1 }, // Bas droit réduit
+      // Corners with padding
+      { x: x + offset - padding, y: y + offset - padding }, // Top-left
+      { x: x + offset + hitboxSize - 1 + padding, y: y + offset - padding }, // Top-right
+      { x: x + offset - padding, y: y + offset + hitboxSize - 1 + padding }, // Bottom-left
+      {
+        x: x + offset + hitboxSize - 1 + padding,
+        y: y + offset + hitboxSize - 1 + padding,
+      }, // Bottom-right
+
+      // Midpoints with padding
+      { x: x + offset + hitboxSize / 2, y: y + offset - padding }, // Top-center
+      {
+        x: x + offset + hitboxSize / 2,
+        y: y + offset + hitboxSize - 1 + padding,
+      }, // Bottom-center
+      { x: x + offset - padding, y: y + offset + hitboxSize / 2 }, // Left-center
+      {
+        x: x + offset + hitboxSize - 1 + padding,
+        y: y + offset + hitboxSize / 2,
+      }, // Right-center
     ];
 
     // Vérifier si un des points touche un obstacle
