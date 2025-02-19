e1ca891c5798aae3377c354cf567df300edfad45
vlouvier <louvier.valentyn@gmail.com>
2025-02-19 11:04:14 +0100

    main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ for (let i = 0; i < coordinatesLength; i++) {
 }
 
 let { x, y } = findPlayerPosition();
-player = new PlayerEntity(10, x * 64, y * 64); // Initialize player entity with position based on tilemap
+player = new PlayerEntity(10, x * 64, y * 64, gameLogicSystem.entities); // Initialize player entity with position based on tilemap
 gameLogicSystem.addEntity(player);
 gameLogicSystem.addSystem(new RenderSystem());
 gameLogicSystem.addSystem(movementSystem);
