aa90f6bb8d636e25829b20349ce6ec2227d6fe97
SpauriRosso <spy456@zoho.com>
2025-02-20 13:39:26 +0100

    Updated : added mock files

---
+++ Components/PauseComponnent.js
@@ -0,0 +1,5 @@
+export default class PauseComponnent {
+    constructor() {
+        this.pause = false
+    }
+}
---
+++ systems/PauseSystem.js
@@ -0,0 +1,7 @@
+export default class PauseSystem {
+    constructor() {
+    //     SOME LOGIC FOR MORE FUUUNNN
+    }
+
+    createPause() {}
+}
