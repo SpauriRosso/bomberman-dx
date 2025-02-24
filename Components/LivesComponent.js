cdb26bd853c81f37e4d01ddf858ed84efd9fe2fc
vlouvier <louvier.valentyn@gmail.com>
2025-02-24 14:22:33 +0100

    Components/

--- Components/LivesComponent.js
+++ Components/LivesComponent.js
@@ -1,5 +1,19 @@
-export default class LivesComponent {
-  constructor(lives) {
-    this.lives = lives;
+export default class LifeComponent {
+  constructor(initialLives) {
+    this.lives = initialLives; // Track the number of lives
+  }
+
+  loseLife() {
+    if (this.lives > 0) {
+      this.lives -= 1; // Decrease the number of lives
+      if (this.lives <= 0) {
+        this.triggerGameOver(); // Trigger game over if no lives left
+      }
+    }
+  }
+
+  triggerGameOver() {
+    console.log("Game Over!"); // Replace with actual game over logic
+    // You can also trigger an event or call a method in your game manager
   }
 }
