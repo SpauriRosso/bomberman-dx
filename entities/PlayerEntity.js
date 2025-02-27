703734b8b53befb204c15f852fa102665be79ad0
clecart <djlike@hotmail.fr>
2025-02-27 10:03:38 +0100

    fix text hud et flash heath

--- entities/PlayerEntity.js
+++
@@ -1,8 +0,0 @@
-import LivesComponent from "../Components/LivesComponent.js";
-
-class PlayerEntity extends Entity {
-  constructor(id, x, y, entities, gameStateEntity) {
-    super(id, x, y, entities, gameStateEntity);
-    this.addComponent("lives", new LivesComponent(3)); // Ajoutez un composant de vie avec 3 vies
-  }
-}
